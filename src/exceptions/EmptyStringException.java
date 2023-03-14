75017cc5df91ba73a651b2e1228a8e169b050bbf
MrSosu <adriani.marco.93@gmail.com>
2023-03-14 17:26:26 +0100

    esempi 14/03

---
+++ src/exceptions/EmptyStringException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class EmptyStringException extends Exception {
+
+    private String message;
+
+    public EmptyStringException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
